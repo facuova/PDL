ba980f26a9f90eeed39459e494e8898cc34aca7d
facuova <facuovalles@gmail.com>
2024-08-08 13:17:56 -0300

    Realizando carrito

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function toggleCart() {
     cartDropdown.style.display = cartDropdown.style.display === 'block' ? 'none' : 'block';
 }
 
+function hideCart() {
+    const cartDropdown = document.getElementById('cart-dropdown');
+    cartDropdown.style.display = cartDropdown.style.display === 'none' ? 'block' : 'none';
+}
+
 function checkout() {
     alert('Procediendo al pago...');
     // Aquí puedes agregar la lógica para el proceso de pago
